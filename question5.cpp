9b2f866ddf4b51ec04f2c4f84e306f4792254c84
dheeraj250041012 <mc250041012@iiti.ac.in>
2025-09-08 15:54:44 +0530

    Update question5.cpp

--- question5.cpp
+++ question5.cpp
@@ -4,9 +4,13 @@
 
 using namespace std;
 
-// TODO: Define Sensor struct
-// struct Sensor {
-// };
+// Define Sensor struct
+struct Sensor {
+    int id;
+    double temperature;
+    double voltage;
+    char status[20]; // fixed-size char array
+};
 
 void print_sensor(int index, int id, double temperature, double voltage, const char* status) {
     cout << "Sensor[" << index << "]: "
@@ -32,15 +36,27 @@ int main(int argc, char* argv[]) {
     input >> num_sensors;
 
     const int MAX_SENSORS = 10;
+    if (num_sensors > MAX_SENSORS) {
+        cerr << "Too many sensors in input (max " << MAX_SENSORS << ")" << endl;
+        return 1;
+    }
 
-    // TODO: Create an array of Sensor
-    // Sensor sensors[MAX_SENSORS];
+    // Create an array of Sensor
+    Sensor sensors[MAX_SENSORS];
 
-    // TODO: Read sensor data from input
-    // for (int i = 0; i < num_sensors; i++) {
-    // }
+    // Read sensor data from input
+    for (int i = 0; i < num_sensors; i++) {
+        input >> sensors[i].id
+              >> sensors[i].temperature
+              >> sensors[i].voltage
+              >> sensors[i].status;
+    }
 
-    // TODO: Iterate using a pointer and print sensor data
+    // Iterate using a pointer and print sensor data
+    Sensor* ptr = sensors;
+    for (int i = 0; i < num_sensors; i++, ptr++) {
+        print_sensor(i, ptr->id, ptr->temperature, ptr->voltage, ptr->status);
+    }
 
     return 0;
 }
